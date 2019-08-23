69bb089056562e091e12b01d13e8d9a314e68ecb
Zalat0y <777zalat0y777@gmail.com>
2019-08-23 17:24:03 +0300

    add expense

--- js/app.js
+++ js/app.js
@@ -79,8 +79,35 @@ class UI {
       }
       this.itemID++;
       this.itemList.push(expense);
+      this.addExpense(expense);
+      // show balance
     }
   }
+
+  // add expence 
+  addExpense(expence) {
+    const div = document.createElement('div');
+    div.classList.add('expense');
+    div.innerHTML = `
+    <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+         <h6 class="expense-title mb-0 text-uppercase list-item">-${expence.title}</h6>
+         <h5 class="expense-amount mb-0 list-item">${expence.amount}</h5>
+
+         <div class="expense-icons list-item">
+
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+           <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+           <i class="fas fa-trash"></i>
+          </a>
+         </div>
+        </div>
+    `;
+    this.expenseList.appendChild(div);
+  }
+
   // total expense 
   totalExpense() {
     let total = 400;
